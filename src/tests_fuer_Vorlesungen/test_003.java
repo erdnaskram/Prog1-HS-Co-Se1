d48fab974b4c62650676335aac9b409a598733f9
erdnaskram <erdnaskram@users.noreply.github.com>
2020-11-09 10:38:54 +0100

    div. Tests für Vorlesung

---
+++ src/tests_fuer_Vorlesungen/test_003.java
@@ -0,0 +1,13 @@
+package tests_fuer_Vorlesungen;
+
+public class test_003 {
+    public static void main(String[] args) {
+        int x =0;
+
+        do{
+            if (x==0)
+                x=7;
+            System.out.println("x = " + x);
+        } while(x++>0);
+    }
+}
