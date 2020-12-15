c33d5aca50caa1b84694deac3f2bfd7ed22a0941
erdna <erdnaskram@gmx.de>
2020-12-15 23:09:02 +0100

    Kap 4.1 Beginn

---
+++ src/kap4/ProgU4_01.java
@@ -0,0 +1,20 @@
+package kap4;
+
+public class ProgU4_01 {
+    public static void main(String[] args) {
+
+        final int ARRAY_SIZE = 10;
+        float[] fractions = new float[ARRAY_SIZE];
+
+        System.out.println("\nMoin und Willkommen beim Anweisungs Gelaber ^^");
+
+        for (float fraction : fractions) {
+            fraction = 0;
+        }
+
+
+
+
+
+    }
+}
